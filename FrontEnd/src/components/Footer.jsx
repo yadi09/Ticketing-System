37611aab075dbde61c640e1a031a6cffda6fcc7b
Yadamzer Terefe <140100340+yadi09@users.noreply.github.com>
2025-03-07 22:55:20 +0300

    Update Footer.jsx

--- FrontEnd/src/components/Footer.jsx
+++ FrontEnd/src/components/Footer.jsx
@@ -4,7 +4,7 @@ class Footer extends Component {
     render() {
         return (
             <footer className="flex justify-between items-center p-4 border-t bg-gray-200 text-black">
-                <span>Copyright © 2025 Elhio telecom. All Rights Reserved.</span>
+                <span>Copyright © 2025 Saas solution. All Rights Reserved.</span>
                 <div className="flex space-x-4">
                     <a href="https://facebook.com" className="text-blue-600">Facebook</a>
                     <a href="https://twitter.com" className="text-blue-600">Twitter</a>
