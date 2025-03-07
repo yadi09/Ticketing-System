a29b37879cb5b026caf4e3794a8ea9fbe086e625
Yadamzer <yadamzerterefe09@gmail.com>
2025-03-07 23:26:39 +0300

    .

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -6,25 +6,11 @@ import connectDB from "./config/db.js";
 import userRoutes from "./routes/userRoutes.js";
 import ticketRoutes from "./routes/ticketRoutes.js";
 import { errorHandler } from "./middlewares/errorHandler.js";
-import path from 'path';
-import { fileURLToPath } from 'url';
 
 dotenv.config();
 connectDB();
 
 const app = express();
-
-// Get the current directory path
-const __filename = fileURLToPath(import.meta.url);  // Convert URL to file path
-const __dirname = path.dirname(__filename);  // Get the directory name
-
-// Serve static files from the frontend (React) build folder
-app.use(express.static(path.join(__dirname, 'FrontEnd', 'dist')));
-
-// Catch-all route to serve index.html for all non-API requests
-app.get('*', (req, res) => {
-    res.sendFile(path.resolve('FrontEnd', 'dist', 'index.html'));
-});
 
 // Middleware
 app.use(express.json());
